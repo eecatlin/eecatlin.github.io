ef343380318b6a7886fbc0b899a92c347b577b4b
Emma Catlin <emma_catlin@brown.edu>
2017-03-08 17:29:35 -0500

    update home page, and in readme links to bio page

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,14 @@ $('#siteNav').affix({
 		top: 100
 	}
 })
+function displayTranslation() {
+	if ($('.english-version').css('display') =='none') {
+		$('.english-version').show();
+		$('.spanish-version').hide();
+		$('#translationTxt').text('Show original');
+	} else {
+		$('.english-version').hide();
+		$('.spanish-version').show();
+		$('#translationTxt').text('View english translation');
+	}
+}
